a5c28547fc9eb62e9204a2cd05d2ab92f305043e
rldnd <gi981226@gmail.com>
2023-02-10 21:11:19 +0900

    feat(riverpod): streamProvider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:study_riverpod/layout/default_layout.dart';
 import 'package:study_riverpod/screens/future_provider_screen.dart';
 import 'package:study_riverpod/screens/state_notifier_provider_screen.dart';
 import 'package:study_riverpod/screens/state_provider_screen.dart';
+import 'package:study_riverpod/screens/stream_provider_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -42,6 +43,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('FutureProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const StreamProviderScreen(),
+                ),
+              );
+            },
+            child: const Text('StreamProviderScreen'),
           ),
         ],
       ),
---
+++ lib/screens/stream_provider_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:study_riverpod/layout/default_layout.dart';
+import 'package:study_riverpod/riverpod/stream_provider.dart';
+
+class StreamProviderScreen extends ConsumerWidget {
+  const StreamProviderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(multipleStreamProvider);
+
+    return DefaultLayout(
+      title: 'StreamProviderScreen',
+      body: Center(
+        child: state.when(
+          data: (data) => Text(data.toString()),
+          error: (err, stack) => Text(err.toString()),
+          loading: () => CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
