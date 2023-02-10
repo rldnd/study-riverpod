d41f07ab05e7d8e8a04dffe18517525190b240a1
rldnd <gi981226@gmail.com>
2023-02-10 21:05:10 +0900

    feat(riverpod): futureProvider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:study_riverpod/layout/default_layout.dart';
+import 'package:study_riverpod/screens/future_provider_screen.dart';
 import 'package:study_riverpod/screens/state_notifier_provider_screen.dart';
 import 'package:study_riverpod/screens/state_provider_screen.dart';
 
@@ -31,6 +32,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('StateNotifierProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const FutureProviderScreen(),
+                ),
+              );
+            },
+            child: const Text('FutureProviderScreen'),
           ),
         ],
       ),
