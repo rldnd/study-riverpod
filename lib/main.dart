61bd96f0e98ba9fb22801d1d741c009a1095231f
rldnd <gi981226@gmail.com>
2023-02-12 19:14:02 +0900

    feat(riverpod): observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:study_riverpod/riverpod/provider_observer.dart';
 import 'package:study_riverpod/screens/home_screen.dart';
 
 void main() {
   runApp(
     ProviderScope(
+      observers: [Logger()],
       child: MaterialApp(
         home: HomeScreen(),
       ),
